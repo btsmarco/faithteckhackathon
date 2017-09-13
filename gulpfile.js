a9bcfc6acedb1537d92f6fbb6739ed04f4a3d5c5
Tim Lemke <timlemkeguitarist@gmail.com>
2017-09-13 16:34:35 -0400

    Updated Gulp Boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,44 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 //A Task to Compile Sass. 
 
 gulp.task('styles', () => {
-	return gulp.src('./dev/styles/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('styles.css'))
-		.pipe(gulp.dest('./public/styles/'))
+  return gulp.src('./dev/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/styles'))
+    .pipe(reload({stream: true}));
 });
 
-//A Task to Compile Javascript
-
 gulp.task('scripts', () => {
-	return gulp.src('./dev/scripts/main.js')
-	//return gulp.src('.dev/scripts/**/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('./public/scripts'))
+  return gulp.src('./dev/scripts/main.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./public/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: '.'  
+  })
 });
 
 //A Task to Watch For Changes to Files
 //A Task to Watch All Other Tasks
 
-gulp.task('watch', () => {
-	gulp.watch('./dev/styles/**/*.scss', ['styles']);
-	gulp.watch('./dev/scripts/main.js', ['scripts']);
+gulp.task('watch', function() {
+  gulp.watch('./dev/scripts/*.js', ['scripts']);
+  gulp.watch('./dev/styles/*.scss', ['styles']);
+  gulp.watch('*.html', reload);
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
 
